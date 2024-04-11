1d6ae51bec31d257ff724212b1e233ac78928ff9
StolidCreator84 <72639009+Guest-615695028@users.noreply.github.com>
2024-04-11 13:25:08 +0800

    Create funcs.go

---
+++ funcs.go
@@ -0,0 +1,78 @@
+package comp
+
+import (
+	"math"
+	"math/cmplx"
+)
+
+// Absolute valueReal
+func Abs(v any) any {
+	switch a := v.(type) {
+	case complex64:
+		return cmplx.Abs(complex128(a))
+	case complex128:
+		return cmplx.Abs(a)
+	case int:
+		return Cond(a < 0, -a, a)
+	case int8:
+		return Cond(a < 0, -a, a)
+	case int16:
+		return Cond(a < 0, -a, a)
+	case int32:
+		return Cond(a < 0, -a, a)
+	case int64:
+		return Cond(a < 0, -a, a)
+	case float32:
+		return math.Abs(float64(a))
+	case float64:
+		return math.Abs(a)
+	case uint, uint8, uint16, uint32, uint64, uintptr:
+		return a
+	default:
+		panic(a)
+	}
+}
+
+func Bool(b any) bool {
+	switch a := b.(type) {
+	case bool:
+		return a
+	case string:
+		return a != ""
+	case int, int8, int16, int32, int64,
+		uint, uint8, uint16, uint32, uint64, uintptr,
+		float32, float64, complex64, complex128:
+		return a != 0
+	case interface{ Bool() bool }:
+		return a.Bool()
+	default:
+		return a != nil
+	}
+}
+
+// Operator ?:
+func Cond(cond, t, f any) any {
+	if Bool(cond) {
+		return t
+	} else {
+		return f
+	}
+}
+
+// Operator ??
+func Nil(v any, w any) any {
+	if v != nil {
+		return v
+	} else {
+		return w
+	}
+}
+
+// Obtain the value if no error, or the error if one
+func Value(v any, err error) any {
+	if err == nil {
+		return v
+	} else {
+		return err
+	}
+}
